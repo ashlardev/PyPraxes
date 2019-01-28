656756001ed07a3eebc395c03ec8a19a4e2eccb5
Christopher Ford <ChristopherFord@pvh.com>
2019-01-28 16:39:31 -0500

    Made changes so that it would also decode the message.

--- secret_msgs/secretmsgs.py
+++ secret_msgs/secretmsgs.py
@@ -1,43 +1,39 @@
+
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 numbers = '0123456789'
 newMessage = ''
-newMessage2 = ''
-newMessage3 = ''
+oldMessage = ''
 key = 3
 key2 = 5
 key3 = 10
 
+
 message = input('Please enter a message: ')
-print('Your original message is', message)
 for character in message:
     if character in alphabet:
         position = alphabet.find(character)
         newPosition = (position + key) % 26
-        newCharacter = alphabet[newPosition]
+        newPosition2 = (newPosition + key2) % 26
+        newPosition3 = (newPosition2 + key3) % 26
+        newCharacter = alphabet[newPosition3]
         #print('The new character is:', newCharacter)
         newMessage += newCharacter
     else:
         newMessage += character
-#print('Your new message is', newMessage)
+
+print('Your original message is', message)
+print('Your new message is', newMessage)
 
 for character in newMessage:
     if character in alphabet:
         position = alphabet.find(character)
-        newPosition = (position + key2) % 26
+        newPosition3 = (position - key3) % 26
+        newPosition2 = (newPosition3 - key2) % 26
+        newPosition = (newPosition2 - key) % 26
         newCharacter = alphabet[newPosition]
         #print('The new character is:', newCharacter)
-        newMessage2 += newCharacter
+        oldMessage += newCharacter
     else:
-        newMessage2 += character
-#print('Your new message is', newMessage2)
+        oldMessage += character
 
-for character in newMessage2:
-    if character in alphabet:
-        position = alphabet.find(character)
-        newPosition = (position + key3) % 26
-        newCharacter = alphabet[newPosition]
-        #print('The new character is:', newCharacter)
-        newMessage3 += newCharacter
-    else:
-        newMessage3 += character
-print('Your new message is', newMessage3)
+print('Your old message is', oldMessage)
